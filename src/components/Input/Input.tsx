e38d4b0063f22e240fc080647a40ef8e8be81636
sapato social <samuelcney2@gmail.com>
2024-10-21 20:13:25 -0300

    Adjustments

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -11,7 +11,7 @@ interface InputProps{
 
 export default function Input({onchange, value, placeholder, icon, name, type}: InputProps){
     return(
-        <div className="w-[440px] h-[55px] flex rounded-md mb-1 border-gray-300 border bg-white items-center p-4">
+        <div className="w-[440px] h-[55px] flex rounded-md mb-5 border-gray-300 border bg-white items-center p-4">
         <input className="w-full h-full focus:outline-none"
         value={value} placeholder={placeholder} onChange={onchange} type={type} name={name}/>
         {   icon ? icon : ''}
